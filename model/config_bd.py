db08e63bc0a7b2ae18288d04afe4a2471f9b55f6
Adrian <adrian2000tg3@gmail.com>
2025-11-06 19:16:27 +0100

    Cambios compilados

--- model/config_bd.py
+++ model/config_bd.py
@@ -2,9 +2,8 @@ import sqlite3
 import os
 
 # Ruta absoluta a la raíz del proyecto
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-ROOT_DIR = os.path.abspath(os.path.join(BASE_DIR, ".."))  # sube una carpeta
-DB_PATH = os.path.join(ROOT_DIR, "configuracion.db")
+
+DB_PATH = os.path.join("configuracion.db")
 
 # === CONEXIÓN ===
 def get_connection():
